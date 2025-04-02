089d16bd255364e29c35cd31874dbb5a2e989574
Magne Rekdal <magne.rekdal@helse-bergen.no>
2025-04-02 14:23:01 +0200

    Initial commit

---
+++ SqlUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+namespace Ablefish.StringUtils
+{
+    public class SqlUtils
+    {
+        public static string TextToSql(string? inputText)
+        {
+            if (inputText == null) return "NULL";
+            var outputString = inputText.Replace((char)39, '´').Trim();
+            if (outputString.Length == 0) return "NULL";
+            return string.Concat("'", outputString, "'");
+        }
+        public static string DateToSql(DateOnly? dateOnly)
+        {
+            if (dateOnly == null) return "NULL";
+            return string.Concat("'", dateOnly.Value.ToString("yyyy-MM-dd"), "'");
+        }
+        public static string IntToSql(int? nullableInt)
+        {
+            return nullableInt?.ToString() ?? "NULL";
+        }
+        public static string DecimalToSql(decimal? nullableDecimal)
+        {
+            return nullableDecimal?.ToString(CultureInfo.InvariantCulture) ?? "NULL";
+        }
+    }
+}
